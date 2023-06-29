14e75ca6c8869e4304d9dc35030e0eeaa28e7ef4
tony-brian <btoni3823@gmail.com>
2023-06-29 11:24:43 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,73 @@
+function add(a, b):
+    return a + b
+
+    let number1 = 5;
+    let number2 = 3;
+    let result = add(number1, number2);
+    console.log(result);
+    
+
+ function subtract(a, b);
+    return a - b
+
+    let number1 = 10;
+    let number2 = 5;
+    let result = subtract(number1, number2);
+    console.log(result);
+
+    function multiply(a, b):
+       return a * b
+
+       let number1 = 3;
+       let number2 = 4;
+       let result = multiply(number1, number2);
+       console.log(result);
+
+
+
+       function divide(a, b):
+       return a / b
+
+       let number1 = 10;
+       let number2 = 2;
+       let result = divide(number1, number2);
+       console.log(result);
+          
+
+       function increment(n) {
+        return n + 1;
+      }  
+
+      let number = 5;
+let result = increment(number);
+console.log(result);
+
+function decrement(n) {
+    return n - 1;
+  }
+
+  let number = 10;
+  let result = decrement(number);
+  console.log(result);
+
+  function parseInteger(n) {
+    return parseInt(n, 10);
+  }
+
+  let number = "42";
+  let parsedNumber = parseInteger(number);
+  console.log(parsedNumber);
+
+  function parseInteger(n) {
+    const parsedInt = parseInt(n, 10);
+    return isNaN(parsedInt) ? NaN : parsedInt;
+  }
+
+  function parseFloatNumber(n) {
+    const parsedFloat = parseFloat(n);
+    return isNaN(parsedFloat) ? NaN : parsedFloat;
+  }
+       
+
+
+
